ec60ff3091e8021c2cb456f1135fa02b89e1b2a4
Yuqing Wang <yuqingwang42@gmail.com>
2022-12-25 23:38:08 +0000

    test 1 passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(input, elm) {
+  let args = Array.from(input);
+  const index = args.indexOf(elm);
+  args.splice(index, 1);
+  return args
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3))
 
 // Do not edit below this line
 module.exports = removeFromArray;
