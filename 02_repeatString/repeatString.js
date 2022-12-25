5452776820b700c61cc55040773150f408c6846c
Yuqing Wang <yuqingwang42@gmail.com>
2022-12-25 22:14:37 +0000

    ex2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+  let res = ''
+  for (i=0; i<num; i++) {
+    res += text;
+  }
+  return res
 };
 
 // Do not edit below this line
