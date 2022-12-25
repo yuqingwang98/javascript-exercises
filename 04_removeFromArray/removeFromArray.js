588b35ee0666f3b98244ae9aae8b2bcdd9ed9e79
Yuqing Wang <yuqingwang42@gmail.com>
2022-12-25 23:49:10 +0000

    this does have python vibe

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,14 @@
-const removeFromArray = function(input, elm) {
-  let args = Array.from(input);
-  const index = args.indexOf(elm);
-  args.splice(index, 1);
-  return args
+const removeFromArray = function(...args) {
+  const array = args[0];
+  const newArray = [];
+  array.forEach((item) => {
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+  return newArray;
 };
 
-console.log(removeFromArray([1, 2, 3, 4], 3))
 
 // Do not edit below this line
 module.exports = removeFromArray;
