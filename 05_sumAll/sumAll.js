5dfc9474d218e9eb6689bd76fa7e69562ffa1123
Yuqing Wang <yuqingwang42@gmail.com>
2022-12-26 00:05:28 +0000

    learnt about number.isinteger

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, finish) {
+  if (start<0 || finish<0 ||!Number.isInteger(start) ||!Number.isInteger(finish)){
+    return 'ERROR'
+  }
+  if (start>finish) {
+    var temp = start
+    var start = finish
+    finish = temp
+  }
 
+  let res = start;
+  for (i=start; i<finish; i++) {
+    res = res + i + 1
+  }
+  return res
 };
+
+console.log(sumAll('s', 1))
 
 // Do not edit below this line
 module.exports = sumAll;
