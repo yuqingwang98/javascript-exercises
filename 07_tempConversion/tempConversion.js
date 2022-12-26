6439fe26251d33a062676ba0351242936796a060
Yuqing Wang <yuqingwang42@gmail.com>
2022-12-26 00:14:27 +0000

    done with this part

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  res = (num-32)* (5/9)
+  return Math.round(res * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  res = num * (9/5) + 32
+  return Math.round(res * 10) / 10
 };
 
 // Do not edit below this line
