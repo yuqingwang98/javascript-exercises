833d7caedcd479d689ce93a401f1534326156c9a
Yuqing Wang <yuqingwang42@gmail.com>
2022-12-29 01:27:54 +0000

    finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+  const cleanString = input.toLowerCase().replace(/[^a-z]/g, "");
+  return (
+    cleanString
+      .split("")
+      .reverse()
+      .join("") == cleanString
+  );
 };
 
 // Do not edit below this line
