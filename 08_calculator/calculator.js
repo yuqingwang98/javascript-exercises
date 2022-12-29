464fd10482747a4487b9799c4bcc62972c6a1ad9
Yuqing Wang <yuqingwang42@gmail.com>
2022-12-29 01:25:05 +0000

    calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
-};
-
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const add = function(num1, num2) {
+  return num1+num2
 
 };
 
-const power = function() {
-	
+const subtract = function(num1, num2) {
+  return num1-num2
 };
 
-const factorial = function() {
-	
+const sum = function(arr) {
+  let total = 0;
+  if(arr.length == 0) {
+    return 0
+  }
+  for (var i in arr) {
+  total += arr[i];
+  }
+  return total
+};
+
+const multiply = function(arr) {
+  let total = 1;
+  for (var i in arr) {
+    total *= arr[i];
+    }
+  return total
+};
+
+const power = function(num1, num2) {
+  return num1**num2
+};
+
+const factorial = function(num) {
+    // if num is 0 or 1, return 1
+    var rval=1;
+    for (var i = 2; i <= num; i++)
+        rval = rval * i;
+    return rval;
 };
 
 // Do not edit below this line
