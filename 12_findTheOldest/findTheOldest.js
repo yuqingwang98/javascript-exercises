43bd4cc16140a6e657b85ca8e8534ed99ca4e505
Yuqing Wang <yuqingwang42@gmail.com>
2022-12-29 01:44:30 +0000

    done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  // The reducer walks through the array element-by-element
+  return array.reduce((oldest, currentPerson) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+    return oldestAge < currentAge ? currentPerson : oldest;
+  })
+};
 
+const getAge = function(birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
 };
 
 // Do not edit below this line
